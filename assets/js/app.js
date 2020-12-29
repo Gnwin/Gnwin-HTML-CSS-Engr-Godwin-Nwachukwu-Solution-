fc71d2945dc46c1df0916a2780c519429e694110
Gnwin <nwgwin@gmail.com>
2020-12-29 13:46:32 +0100

    add initial project files

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+
+	let navicon = document.getElementById('nav-icon1');
+	// console.log(navicon);
+	navicon.addEventListener('click', function(){
+		this.classList.toggle('open');
+	})
+
+
+// window.onload
+
+// alert('js connected')
