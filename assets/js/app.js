059dafac5f9f2afdab7af8db4eb994ab30d5011a
Gnwin <nwgwin@gmail.com>
2021-01-02 17:32:15 +0100

    make carousel functional

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,53 +20,44 @@ console.log(slides.children);
 console.log(dots);
 
 var num = 0;
-
 dots[num].classList.add('active');
-function slide(){
-	var remove = slides.removeChild(slides.children[0]);
+
+var remove;
+
+function slide(n){
+	remove = slides.removeChild(slides.children[n]);
 	for (let i = 0; i < dots.length; i++){
 		dots[i].classList.remove('active');
 	}
 	num++;
-		if (num === dots.length ) {
-			// dots[num].classList.remove('active');
-			dots[0].classList.add('active');
-			slides.appendChild(remove);
-			num = 0;
-			return;
-		}
+	if (num === dots.length) {
+		dots[n].classList.add('active');
+		slides.appendChild(remove);
+		num = n;
+		return;
+	}
 	dots[num].classList.add('active');
 	slides.appendChild(remove);
 };
 
 
-
-// }
-
-// function slide(){
-// 	let first = slides.children[0];
-// 	console.log(first);
-// 	for (let i = 0; i < slides.children.length; i++) {
-// 		if (i === slides.children.length - 1){
-// 			slides.children[i] = first;
-// 			break;
-// 		}
-// 		slides.children[i] = slides.children[i + 1];
-// 	}
-// 	// console.log(arr);
-// 	// return arr;
-// }
-// 	var remove = slides.removeChild(slides.children[0]);
-// 	slides.appendChild(remove);
-// }
-
-
-// function myFunction() {
-//   var list = document.getElementById("myList");
-//   var remove = list.removeChild(list.childNodes[0]);
-//   var demo = document.querySelector('#demo')
-//   demo.appendChild(remove);
-// }
+function slideOne(){
+	if (num === 0){
+		dots[num].classList.remove('active');
+		dots[dots.length-1].classList.add('active');
+		remove = slides.removeChild(slides.children[slides.children.length - 1]);
+		slides.insertBefore(remove, slides.childNodes[0]);
+		num = dots.length-1;
+		return;
+	}
+	num--
+	for (let i = 0; i < dots.length; i++){
+		dots[i].classList.remove('active');
+	}
+	remove = slides.removeChild(slides.children[slides.children.length - 1]);
+	dots[num].classList.add('active');
+	slides.insertBefore(remove, slides.childNodes[0]);
+};
 
 
 
